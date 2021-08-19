b99eee21932defba912895ec57783508cf88526f
kangalioo <jannik.a.schaper@web.de>
2021-08-19 14:16:50 +0200

    push

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+fn formatting_is_broken_too()
+{
+    if { true } { /* oh and trigger clippy */}
+
+    use std::os::unix; // and this will only be caught when running on windows
+}
+
+#[cfg(feature = "myfeature")]
+ThisWillOnlyFailToCompileIfMyfeatureIsIncluded;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_doesnt_work() {
+        assert_eq!(2 + 2, 3);
+    }
+}
