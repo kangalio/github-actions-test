3aab7cc4a2da9a3e950c2dd71b3dbe1453d12434
kangalioo <jannik.a.schaper@web.de>
2021-08-19 14:51:51 +0200

    e

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ fn formatting_is_broken_too()
     if { true } { /* oh and trigger clippy */}
 
     use std::os::unix; // and this will only be caught when running on windows
+
+    std::time::Duration::ZERO; // will fail on pre-1.53 rust versions (for testing MSRV)
 }
 
 #[cfg(not(feature = "myfeature"))]
